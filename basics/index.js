97e7fbe44ab6ee38ff899109186b2dfa4297f798
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-10-07 16:45:57 +0530

    first commit

---
+++ basics/index.js
@@ -0,0 +1,15 @@
+console.log('Hi Hello');
+
+// console.log('Window: ', window);
+
+// console.log('Document: ', window.document); // nothing but the HTML Document loaded in the browser
+
+// console.log('Document: ', document); // nothing but the HTML Document loaded in the browser, the part of the screen which show the marked up contents
+
+console.log('a,b,c,d,e,f,g,h,i'.split(','));
+
+// this is same as line 10
+// window.alert('Hi B52WETAMIL, I am Alert');
+// this is same as line 8, when window is defined automatically its methods/props are defined like window
+// alert('Hi B52WETAMIL, I am Alert');
+
